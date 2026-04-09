2c0ecd6e78ce97a68a483da7a76c5a18c6a89695
xiabarun001 <xiabarun001@gmail.com>
2026-04-09 14:53:10 +0800

    feat: setup react and electron scaffold with gitignore

---
+++ electron/main.js
@@ -0,0 +1,33 @@
+﻿const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
+    mainWindow.webContents.openDevTools();
+  } else {
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Hello Study-abroad</h1>
+      <p>Dashboard Grid UI will be built here.</p>
+    </div>
+  );
+}
+
+export default App;
